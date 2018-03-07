4407ea61ac962f97b9b2be7cfed4839145a053aa
shivamsrivastava1 <shivamsrivastava2501@gmail.com>
2018-03-07 23:43:26 +0530

    First commit

---
+++ java1/src/java1/App.java
@@ -0,0 +1,30 @@
+package java1;
+
+public class App {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort=525;
+		long myLong =2456;
+		double Double=1.2345;
+		float myFloat=234.4f;
+		char myChar='y';
+		boolean myBoolean=true;
+		byte myByte=127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(Double);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+		
+		
+		
+
+	}
+
+}
